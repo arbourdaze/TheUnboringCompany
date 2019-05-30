5f4a94630f4d5a76920e9aa470122257e36d5755
Christopher Luikart <christopherluikart@Christopher-Luikarts-MacBook.local>
2019-05-30 17:42:27 -0400

    Middleware rough draft

--- Middleware.py
+++ Middleware.py
@@ -1,11 +1,28 @@
 #Middleware for the AI bot
+from ibm_watson import DiscoveryV1
 
-#Get Data from front end
 
-#Process Data
+class Searcher:
 
-#Query Discovery and get results
+    discovery = DiscoveryV1(version={version},iam_apikey={apikey},url={url})
 
-#Parse results
 
-#Return results to front end
+    def __init__ (self):
+    
+
+    def makeQuery(self, responses):
+
+        if not isinstance(responses, dict):
+            print("Responses should in format of python dict. {Question : Response} ")
+            exit(-1)
+
+        result = None
+
+        #Query Fields{env_id, col_id, filter, query, nl_query, passages,
+        #  agg, count, ret_fields, offset, sort, highlight, 
+        # pass_fields, pass_chars, deduplicate, dedup_field,
+        #  coll_ids, similar, sim_doc_ids, sim_fields, bias,
+        #  log_opt_out}
+        
+
+        return result
