f3c96b57b9b5682607a4a8acaa66ce269ad81eef
WKLeee <lee.7832@osu.edu>
2019-06-10 04:53:28 -0400

    added time to minutes, get movies info from watson response

--- Middleware.py
+++ Middleware.py
@@ -1,11 +1,15 @@
 #Middleware for the AI bot
 from ibm_watson import DiscoveryV1
+import json
+import data_processing as dp
+import config as cf
 
-version = ""
-apikey = ""
-url = ""
-env_id = ""
-col_id = ""
+
+version = "'2018-08-01'"
+apikey = "gAmMFF4c5rrU5pPXMgIQhXwJ3HeKGgqWYv1WhzX3yJm7"
+url = "https://gateway.watsonplatform.net/discovery/api"
+env_id = "33c919eb-2486-412f-9936-13c492f0aa6b"
+col_id = "b1e74dd2-867b-41c9-8565-adc76d396f93"
 
 
 def middleware(responses, mood):
@@ -15,17 +19,17 @@ def middleware(responses, mood):
     #Format into keywords
 
 
-    for each topic:
-        if response is not no:
-            results = makeQuery(keywords)
-            #save top 3
+    #for each topic:
+    #    if response is not no:
+    #        results = makeQuery(keywords)
+    #        #save top 3
 
     #Convert json objects into something not terrible for reading
 
 
 def makeQuery(keywords, discovery):
     result = None
-    num_results = 3
+
 
     """
     filter = excludes docs that don't mention filter
@@ -38,7 +42,38 @@ def makeQuery(keywords, discovery):
 
 
     result = discovery.query(environment_id = env_id, collection_id = col_id, filter = None,
-        query = keywords, count = num_results)
+        query = keywords, count = cf.NUM_RESULTS)
         
 
     return result
+
+
+# get bored minutes
+def get_minutes(data):
+    hours, minutes = dp.get_time(data)
+    result = hours * 60 + minutes
+
+    return result
+
+
+# get movies infos from watson response
+def get_movies(response):
+    movies = []
+    matching_results = response.result["matching_results"]
+    if cf.NUM_RESULTS < matching_results:
+        num = cf.NUM_RESULTS
+    else:
+        num = matching_results
+    for i in range(num):
+        title = response.result["results"][i]['Title']
+        genre = response.result["results"][i]['Genre']
+        summary = response.result["results"][i]['Summary']
+        runtime = response.result["results"][i]["Runtime"]
+        movies.append({'title':title, 'genre':genre, 'summary':summary, 'runtime':runtime})
+       
+    return movies
+
+
+if __name__=="__main__":
+    data = dp.load_file(cf.DATA_PATH)
+    print(get_minutes(data))
