abe006c631047e8bfb033d500b3572644824ca29
WKLeee <lee.7832@osu.edu>
2019-06-12 17:02:55 -0400

    Updated middle

--- Middleware.py
+++ Middleware.py
@@ -1,7 +1,6 @@
 #Middleware for the AI bot
 from ibm_watson import DiscoveryV1
 import json
-import data_processing as dp
 import config as cf
 import secrets as sec
 
@@ -16,6 +15,7 @@ def middleware(responses, mood):
     timeLimit = get_minutes(data)
 
     results = [] #Object containing the json objects of all results returned
+    correctList = []
     #Parse into keywords and make query
     for topic in data["Topics"]:
         keywords = []
@@ -25,33 +25,32 @@ def middleware(responses, mood):
         for catagory in data["Liked" + topic]: #Temporary, based on what rebecca decides to do
             keywords.append(catagory)
 
-        result = makeQuery(keywords, discovery, topic)
+        response = makeQuery(keywords, discovery, topic)
+        res = response.result["results"];
 
-        jobj = result.json()
-
-        #Filter by time
-        for element in jobj["results"]:
-            if int(element["Runtime"]) > timeLimit:
-                jobj["results"].remove(element)
-        results.append(jobj)
+        time_filter(res, timeLimit, correctList)
 
     #Convert json objects into something not terrible for reading
-    return get_response(results)
+    return get_response(correctList)
 
 def makeQuery(keywords, discovery, topic):
     result = None
 
-    # filterParam = "extracted_metadata.filename:\"" + topic + "\""
+    filterParam = "extracted_metadata.filename:\"" + topic + "\""
 
-    # filter = excludes docs that don't mention filter
-    # query = the query
-    # natural_language_query = the query but using natural language processing
-    # passages = T/F returns most relevant passages from results
-    # aggregation = combines query search with filters
-    # count = # of results to return
+    result = discovery.query(environment_id = sec.env_id, collection_id = sec.col_id, filter = filterParam,
+        query = ' '.join(keywords), count = cf.NUM_RESULTS)
 
-    result = discovery.query(environment_id = sec.env_id, collection_id = sec.col_id, filter = filterParam, query = ' '.join(keywords), count = cf.NUM_RESULTS)
     return result
+
+"""
+    filter = excludes docs that don't mention filter
+    query = the query
+    natural_language_query = the query but using natural language processing
+    passages = T/F returns most relevant passages from results
+    aggregation = combines query search with filters
+    count = # of results to return
+"""
 
 
 # get bored minutes
@@ -69,40 +68,46 @@ def get_minutes(data):
 # get movies infos from watson response
 #[{"Name":Name, "Description":Description}, {"Name":Name, "Description":Description}, ...]
 def get_response(responses):
-
     activities = []
-    for response in responses:
-        for result in response["results"]:
-            if "Movie" in result["extracted_metadata"]["filename"]:
-                title = result["Title"]
-                year = result["Year"]
-                runtime = result["Runtime"]
-                genre = result["Genre"]
-                summary = result["Summary"]
+    for result in responses:
+        if "Movie" in result["extracted_metadata"]["filename"]:
+            title = result["Title"]
+            year = result["Year"]
+            runtime = result["Runtime"]
+            genre = result["Genre"]
+            summary = result["Summary"]
+            name = "Movie: " + title
+            description = year + "; " + runtime + " minutes; " + genre + ";\n" + summary
 
-                name = "Movie: " + title
-                description = year + "; " + runtime + " minutes; " + genre + ";\n" + summary
+            activity = {"Name":name, "Description":description}
+            activity = json.dumps(activity)
+            activity = json.loads(activity)
+            activities.append(activity)
+        else:
+            recipe = result["name"]
+            preptime = int(result["preptime"]) + int(result["cooktime"]) + int(result["waittime"])
+            servings = result["servings"]
+            calories = result["calories"]
+            ingredients = ', '.join(result["ingredients"])
+            instructions = result["instructions"]
 
-                activity = {"Name":name, "Description":description}
-                activity = json.dumps(activity)
-                activity = json.loads(activity)
-                activities.append(activity)
-            else:
-                recipe = result["name"]
-                preptime = int(result["preptime"]) + int(result["cooktime"])
-                servings = result["servings"]
-                calories = result["calories"]
-                ingredients = ', '.join(result["ingredients"])
-                instructions = result["instructions"]
-
-                name = "Recipe: " + recipe
-                description = ("Preptime: " + preptime + "; Servings: "
+            name = "Recipe: " + recipe
+            description = ("Preptime: " + preptime + "; Servings: "
                  + servings + "; Calories: " + calories + ";\nIngredients: "
-                  + ingredients + ";\nInstructions: " + instructions)
-
-                activity = {"Name:":name, "Description":description}
-                activity = json.dumps(activity)
-                activity = json.loads(activity)
-                activities.append(activity)
-
+                 + ingredients + ";\nInstructions: " + instructions)
+            activity = {"Name:":name, "Description":description}
+            activity = json.dumps(activity)
+            activity = json.loads(activity)
+            activities.append(activity)
     return activities
+
+
+def time_filter(res, timeLimit):
+    for result in res:
+        if 'Runtime' in result.keys():
+            if int(result["Runtime"]) < timeLimit:
+                correctList.append(result)
+        elif 'calories' in result.keys():
+            total = result["preptime"]+result["waittime"]+result["cooktime"]
+            if total < timeLimit:
+                correctList.append(result)
