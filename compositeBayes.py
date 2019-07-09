d43503272783ea1a26f65eb94b5745d4b097d8ed
Christopher Luikart <christopherluikart@Christopher-Luikarts-MacBook.local>
2019-07-09 11:02:16 -0400

    Added enforceLength method to help identify a likely bug in the bayes code

--- compositeBayes.py
+++ compositeBayes.py
@@ -9,6 +9,8 @@ class CompositeBayes:
 	descriptionBayes = None
 	weights = []
 	gradient = 0
+	titleDataLength = 0
+	descriptionDataLength = 0
 
 	def __init__(self):
 		self.typeBayes = GaussianNB()
@@ -18,7 +20,26 @@ class CompositeBayes:
 		self.gradient = .03
 
 
+	def enforceLength(dataList, requiredLength = 0):
+		length = requiredLength
+		if requiredLength == 0:
+			length = len(dataList[0])
+		for item in dataList:
+			if len(item) is not length:
+				return False
+		return True
+
 	def compositeFit(self, typeData, titleData, descriptionData, categories):
+
+		if not enforceLength(titleData):
+			print("Error: TitleData length is not consistent")
+		else:
+			self.titleDataLength = len(titleData[0])
+		if not enforceLength(descriptionData):
+			print("Error: DescriptionData length is not consistent")
+		else:
+			self.descriptionDataLength = len(descriptionData[0])
+
 		self.typeBayes.fit(typeData, categories)
 		self.titleBayes.fit(titleData, categories)
 		self.descriptionBayes.fit(descriptionData, categories)
@@ -26,6 +47,12 @@ class CompositeBayes:
 
 	#Returns Predictions
 	def compositePredict(self, typeData, titleData, descriptionData):
+
+		if not enforceLength(titleData, self.titleDataLength):
+			print("Error: Test TitleData length is not consistent")
+		if not enforceLength(descriptionData, self.descriptionDataLength):
+			print("Error: Test DescriptionData length is not consistent")
+
 		typePreds = self.typeBayes.predict(typeData)
 		titlePreds = self.titleBayes.predict(titleData)
 		descriptionPreds = self.descriptionBayes.predict(descriptionData)
